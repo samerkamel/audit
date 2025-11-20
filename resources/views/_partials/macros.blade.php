7ecb948bce4c1ebec7bf245cf32df6e9762e5814
samerkamel <samer.kamel@aura-sys.com>
2025-11-20 15:09:40 +0200

    fix: prevent logo stretching with object-fit contain

--- resources/views/_partials/macros.blade.php
+++ resources/views/_partials/macros.blade.php
@@ -3,4 +3,4 @@
   $height = $height ?? '32';
 @endphp
 
-<img src="{{ asset('assets/img/branding/Logo - Small - Transparent.png') }}" alt="Alfa Electronics" width="{{ $width }}" height="{{ $height }}">
+<img src="{{ asset('assets/img/branding/Logo - Small - Transparent.png') }}" alt="Alfa Electronics" width="{{ $width }}" height="{{ $height }}" style="object-fit: contain;">
